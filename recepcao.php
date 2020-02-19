0c7e28219b1216dba75a2f0def273465aa12534f
Pedro Henrique Delasalle Silva Ribeiro <pedrodelasalle12@gmail.com>
2020-02-19 13:41:12 -0300

    Adição dos arquivos já prontos
    Arquivos já prontos feitos por Luiz.

---
+++ recepcao.php
@@ -0,0 +1,10 @@
+<?php 
+
+if (isset($_POST['recepcao'])&& empty($_POST['recepcao'])
+){
+    echo "O usuario enviou os dados";
+}
+    
+        
+ ?>
+
---
+++ recepcao1.php
@@ -0,0 +1,13 @@
+    <?php 
+
+if (isset($_POST['recepcao'])&& empty($_POST['recepcao'])
+){
+    echo "O usuario enviou os dados";
+}else
+{
+    echo 'O Usuario nao enviou os dados';
+}
+    
+        
+ ?>
+
